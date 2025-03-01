965dd8de2e715b074a826b5f59d72f0189008495
Ihor Yakushev <ihor.yakushev33@mail.com>
2025-03-01 22:17:12 +0200

    Second task is solved!

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int i = 2; i <= 100; i+=2) {
+                System.out.println("IT specialist with id: " + i + " is fired!");
+        }
+
+        int workerCounter = 2;
+        while (workerCounter <= 100) {
+            System.out.println("IT specialist with id: " + workerCounter + " is fired!");
+            workerCounter += 2;
+
+        }
     }
 }
